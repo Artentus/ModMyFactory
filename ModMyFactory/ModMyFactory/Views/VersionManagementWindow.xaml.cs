47641bf0088b65a0a409fe0179fef33866f899bf
Mathis Rech <mathisrech@gmx.de>
2016-10-08 14:44:40 +0200

    Fixed crash when clicking in list view of version manager.

--- ModMyFactory/ModMyFactory/Views/VersionManagementWindow.xaml.cs
+++ ModMyFactory/ModMyFactory/Views/VersionManagementWindow.xaml.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -21,8 +21,9 @@ namespace ModMyFactory.Views
             if (e.ChangedButton != MouseButton.Left)
                 return;
 
+            int itemCount = ((ICollectionView)listBox.ItemsSource).Cast<object>().Count();
             ListBoxItem item = null;
-            for (int i = 0; i < ((ICollection)((ICollectionView)listBox.ItemsSource).SourceCollection).Count; i++)
+            for (int i = 0; i < itemCount; i++)
             {
                 item = (ListBoxItem)listBox.ItemContainerGenerator.ContainerFromIndex(i);
                 if (VisualTreeHelper.GetDescendantBounds(item).Contains(e.GetPosition(item)))
