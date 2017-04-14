44a6e342c6ecefa3ab948f64975d30341a8464cc
Mathis Rech <mathisrech@gmx.de>
2017-04-14 17:18:30 +0200

    Fixed mod group animations.

---
+++ ModMyFactory/ModMyFactory/MVVM/DefaultExpandedExpander.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ModMyFactory.MVVM
+{
+    class DefaultExpandedExpander : Expander
+    {
+        static DefaultExpandedExpander()
+        {
+            IsExpandedProperty.OverrideMetadata(typeof(DefaultExpandedExpander), new FrameworkPropertyMetadata(true));
+        }
+
+        public DefaultExpandedExpander()
+        {
+            IsExpanded = true;
+        }
+    }
+}
