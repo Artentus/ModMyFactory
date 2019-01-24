4624777b755755380c687db82b4cdd217824bf52
HyperUniv <46998486+HyperUniv@users.noreply.github.com>
2019-01-24 22:57:26 +0900

    Update LoginWindow.xaml.cs

--- ModMyFactory/ModMyFactory/Views/LoginWindow.xaml.cs
+++ ModMyFactory/ModMyFactory/Views/LoginWindow.xaml.cs
@@ -1,32 +1,17 @@
-﻿using System.Windows;
+using System.Windows;
 
 namespace ModMyFactory.Views
 {
     partial class LoginWindow
     {
-        public bool useLocalToken;
-        public static bool isOpened = false;
         public LoginWindow()
         {
-            isOpened = true;
             InitializeComponent();
-            useLocalToken = false;
-        }
-        private void UseLocalButtonClickHandler(object sender, RoutedEventArgs e)
-        {
-            useLocalToken = true;
-            isOpened = false;
-            DialogResult = true;
-        }
-        private void OKButtonClickHandler(object sender, RoutedEventArgs e)
-        {
-            isOpened = false;
-            DialogResult = true;
         }
 
-        private void CancelButtonClickHandler(object sender, RoutedEventArgs e)
+        private void OKButtonClickHandler(object sender, RoutedEventArgs e)
         {
-            isOpened = false;
+            DialogResult = true;
         }
     }
 }
