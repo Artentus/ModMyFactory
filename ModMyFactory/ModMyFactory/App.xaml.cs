1833892f7a6ee02f95820e77a6951eca80b7959f
Mathis Rech <mathisrech@gmx.de>
2019-03-09 05:30:29 +0100

    Fixed Factorio crash due to disabled mod file names.

--- ModMyFactory/ModMyFactory/App.xaml.cs
+++ ModMyFactory/ModMyFactory/App.xaml.cs
@@ -19,7 +19,7 @@ namespace ModMyFactory
 {
     public partial class App : Application
     {
-        private const int PreReleaseVersion = 1;
+        private const int PreReleaseVersion = 2;
 
         /// <summary>
         /// The current application instance.
